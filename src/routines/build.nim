aaffe06397254215c09fdaf026bb3e8f559befe0
xTrayambak <xtrayambak@gmail.com>
2025-12-03 15:55:35 +0530

    fix: build: ensure that output destination cannot match source file path
    This'd cause Nim to overwrite the source file for tests with their
    compiled output instead. Oops.
    
    Fixes #10

--- src/routines/build.nim
+++ src/routines/build.nim
@@ -3,7 +3,7 @@
 ## build logic into one set of routines.
 ##
 ## Copyright (C) 2025 Trayambak Rai (xtrayambak@disroot.org)
-import std/[os, osproc, options, tables]
+import std/[os, osproc, options, strutils, tables]
 import
   ../types/[backend, compilation_options, project, toolchain], ../[argparser, output]
 import ./[dependencies, locking, neo_directory]
@@ -129,6 +129,9 @@ proc buildBinaries*(
       "<green>" & binFile & "<reset> using the <blue>" &
         project.package.backend.toHumanString() & "<reset> backend",
     )
+    var binFile = binFile
+    binFile.removeSuffix(".nim")
+
     let stats = toolchain.compile(
       project.package.backend,
       directory / binFile,
